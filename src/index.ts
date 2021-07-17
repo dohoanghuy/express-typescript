b4de11d188fa597a576d1bf2ad3a4820bb51ba7f
Do Hoang Huy <dohoanghuy94@gmail.com>
2021-07-17 15:19:09 +0700

    add jest husky prettier

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => res.send("Express + TypeScript Server"));
+
+app.get("/add", (req: Request, res: Response) => {});
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
