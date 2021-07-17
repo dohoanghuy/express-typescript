e8e10a59b18369123288b27612f8ba64b4c6d32c
Do Hoang Huy <dohoanghuy94@gmail.com>
2021-07-17 21:21:13 +0700

    add dockerfile

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("Express + TypeScript Server"));
 
