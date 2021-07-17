6fc21d3f38dd58ecb14efb691687e1702b480256
Do Hoang Huy <dohoanghuy94@gmail.com>
2021-07-17 20:23:25 +0700

    check lint-stage

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ const PORT = 3000;
 
 app.get("/", (req, res) => res.send("Express + TypeScript Server"));
 
-app.get("/add", (req: Request, res: Response) => {});
+app.get("/add", (req: Request, res: Response) => {
+  res.send("add result");
+});
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
