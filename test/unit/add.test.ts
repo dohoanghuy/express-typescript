12dade2bf9e3c311e5fd67bd56c3114042fd542c
Do Hoang Huy <dohoanghuy94@gmail.com>
2021-07-17 18:49:31 +0700

    add unit test, set vscode auto save

---
+++ test/unit/add.test.ts
@@ -0,0 +1,17 @@
+describe("Sample test", () => {
+  it("should return result success when add two numbers", () => {
+    expect(1 + 1).toEqual(2);
+  });
+
+  it("should return result success when subtract two numbers", () => {
+    expect(1 - 1).toEqual(0);
+  });
+
+  it("should return result success when multi two numbers", () => {
+    expect(1 * 1).toEqual(1);
+  });
+
+  it("should return result success when divide two numbers", () => {
+    expect(1 / 2).toEqual(0.5);
+  });
+});
